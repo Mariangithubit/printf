4a4040451758a22cc4d688d366f3095c5fc75510
Mariangithubit <metsyb1@hotmail.com>
2023-04-25 08:36:57 -0700

    strlen

--- function.c
+++ function.c
@@ -1,41 +1,5 @@
 #include "main.h"
 /**
- * get_specifier - format function
- * @s: the string
- * Return: the number of characters printed
- */
-int (*get_specifier(char *s))(va_list args, p_type *specif)
-{
-	specifier_type func[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"%", p_percent},
-		{"d", p_integer},
-		{"i", p_integer},
-		{"S", p_S},
-		{"b", p_binary},
-		{"o", p_octal},
-		{"u", p_unsigned},
-		{"p", p_address},
-		{"x", p_hex},
-		{"X", p_HEX},
-		{"r", p_rev},
-		{"R", p_rot13},
-		{NULL, NULL}
-	};
-	int n = 0;
-
-	while (func[n].specifier)
-	{
-		if (*s == func[n].specifer[0])
-		{
-			return (func[n].f);
-		}
-		n++;
-	}
-	return (NULL);
-}
-/**
  * get_p_func - print the function
  * @s: the string
  * @args: arguments
--- main.h
+++ main.h
@@ -24,18 +24,26 @@ void init_specif(va_list args, p_type *specif);
 int _putchar(int c);
 int _input(char *st);
 
+/* function.c */
+int get_p_func(char *s, va_list args, p_type *specif);
+int get_modifier(char *s, p_type *specif);
+char *get_width(char *s, va_list args, p_type *specif);
+char *get_precision(char *s, va_list args, p_type *specif);
+int get_flag(char *s, p_type *specif);
+
+/* 1-function.c */
+int (*get_specifier(char *s))(va_list args, p_type * specif);
+
+/* strlen.c */
+int p_strlen(char *begin, char *end, char *except);
+
+
+
 
 int _chars(va_list args, p_type *specif);
 int _string(va_list args, p_type *specif);
 int _percent(va_list args, p_type *specif);
 int _integer(va_list args, p_type *specif);
-int (*get_specifier(char *s))(va_list args, p_type *specif);
-int get_p_func(char *s, va_list args, p_type *specif);
-int get_modifier(char *s, p_type *specif);
-int get _width(char *s, va_list args, p_type *specif);
-int get_flag(char *s, p_type *specif);
-char get_percision(char *s, va_list args, p_type *specif)
-int p_strlen(char *begin, char *end, char *except)
 
 /**
  * struct flag - structure of paramters
